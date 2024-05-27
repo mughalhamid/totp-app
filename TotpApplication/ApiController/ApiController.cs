aeaa30f32c57d4062e7e3b5b11191a3d7aabb1ca
Hamid Mughal <mughal.hamid@gmail.com>
2024-05-27 15:27:18 +0500

    Removed extra files and adjusted few things

--- TotpApplication/ApiController/ApiController.cs
+++ TotpApplication/ApiController/ApiController.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using TotpApplication.Service;
 
 namespace TotpApplication.ApiController
 {
     //private readonly HttpClient _httpClient;
-    public class ApiController
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class APIController : ControllerBase
     {
-        HttpClient _httpClient = new HttpClient();
-        
+        [HttpGet]
+        public IActionResult GenerateSecretKey()
+        {
+            string secretKey = TotpHelper.GenerateSecretKey();
+            return Ok(secretKey);
+        }
+
+        [HttpGet]
+        public IActionResult Validate(string code, string secretKey)
+        {
+            if (TotpHelper.ValidateTotp(secretKey, code))
+            {
+                return Ok(true);
+            }
+            else
+            {
+                return BadRequest("Invalid Code. Please try again");
+            }
+        }
     }
 }
